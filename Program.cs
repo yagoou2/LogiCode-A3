ff5dfe1d4135aacb24dbc88f9d580568f0e512f7
Yago Costa <yagocosta223@gmail.com>
2026-05-13 21:22:24 -0300

    Remove damage handling and update console message

--- Program.cs
+++ Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     static void Main()
     {
-        int vida = 100;
-
-        Console.WriteLine("Você tomou dano!");
-
-        vida = vida - 20;
-
-        Console.WriteLine("Vida restante: " + vida);
+        Console.WriteLine("TEm nada aqui nao");
     }
 }
